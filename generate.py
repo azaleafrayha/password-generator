6c601270609435cd24e28bb6578cbe66d7870b44
azaleafrayha <zalea473@gmail.com>
2026-01-05 22:30:50 +0700

    Finished Project

--- generate.py
+++ generate.py
@@ -3,7 +3,8 @@ import secrets
 import string
 
 
-def generate_password(length, nums, special_chars, uppercase, lowercase):
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+
     # Define the possible characters for the password
     letters = string.ascii_letters
     digits = string.digits
@@ -17,14 +18,23 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
+        
         constraints = [
-            (nums, '[0123456789]')
-        ]        
+            (nums, r'\d'),
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
+        ]
+
+        # Check constraints        
+        if all(
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
+        ):
+            break
+    
     return password
     
-# new_password = generate_password(8)
-# print(new_password)
-
-pattern = '[a-z]t'
-quote = 'Not all those who wander are lost.'
-#print(re.findall(pattern, quote))
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
